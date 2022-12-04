41bc7a6c865fdb5483c78c20b7ec15ae1a4b191c
Lane Humphreys <56990308+lanehump@users.noreply.github.com>
2022-12-03 22:45:50 -0500

    Update

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@(vl-csyy1&wek%6uupgwur*lhzh5ucj792m799j-iriy1*nun'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1','sampledomain.com']
 
