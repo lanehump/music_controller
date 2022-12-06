7d8fcd0ae81c2aff4129db7f517c5a715bac2ddd
Lane Humphreys <56990308+lanehump@users.noreply.github.com>
2022-12-05 22:01:56 -0500

    fix redirect uri

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = "ab2392445c944c46ab172dce64e19e51"
 CLIENT_SECRET = "60f3bbca71d3470fb2e3cd3c164c03ee"
-REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
+REDIRECT_URI = "http://cuemusic.herokuapp.com/spotify/redirect"
